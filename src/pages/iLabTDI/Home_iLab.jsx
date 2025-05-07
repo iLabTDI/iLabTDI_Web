d7bad1ce75e563b4fb456ef38e45961060dd6378
FabianRag5 <hector.rivera2888@alumnos.udg.mx>
2025-05-07 10:32:26 -0600

    Cambios de interfaz

--- src/pages/iLabTDI/Home_iLab.jsx
+++ src/pages/iLabTDI/Home_iLab.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar_iLab from "./components_iLab/NavBar/NavBar_iLab";
 import Banner_iLab from "./components_iLab/Banner/Banner_iLab";
+import About_iLab from "./components_iLab/About/About_iLab";
 
 function Home_iLab() {
   return (
     <div>
       <NavBar_iLab />
       <Banner_iLab />
+      <About_iLab />
     </div>
   );
 }
